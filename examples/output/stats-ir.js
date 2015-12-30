d922a43e3d4e086c87435f52221bc3e4df4a288c
Juan Pablo Alperin <juan@alperin.ca>
2015-12-29 16:54:06 -0800

    tweek css and update stats

--- examples/output/stats-ir.js
+++ examples/output/stats-ir.js
@@ -1,1 +1,88 @@
-../scholar-scraper/stats-ir.js
+var date = "Tue Dec 29 2015 16:47:29 GMT-0800 (PST)"
+var data = [
+  {
+    "name": "Juan Pablo Alperin",
+    "photo": "http://scholar.google.com/citations?view_op=view_photo&user=QW-eo0MAAAAJ&citpid=1",
+    "keywords": [
+      "Scholarly Communication",
+      "Latin America",
+      "Open Access",
+      "Altmetrics"
+    ],
+    "affiliation": "Assistant Professor, Simon Fraser University",
+    "url": "https://scholar.google.com/citations?user=QW-eo0MAAAAJ",
+    "stats": {
+      "citations": [
+        "155",
+        "149"
+      ],
+      "hindex": [
+        "6",
+        "6"
+      ],
+      "i10index": [
+        "4",
+        "4"
+      ],
+      "articles": [
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=18358168313039408295",
+          "40"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=2339300654326134976",
+          "29"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=603210712826963713,6115536236073831064",
+          "24"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=17529319604071970957",
+          "20"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=14015674882563791507",
+          "8"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=7472416283065230354",
+          "6"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=13918265887449564160",
+          "6"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=1308867547326861437",
+          "5"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=2304846227896743763",
+          "4"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=14244773496994540438,2061042163236346089,11442781361128510626",
+          "4"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=1154013045948546741",
+          "3"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=8352684746038389618",
+          "2"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=7663946573571325985",
+          "2"
+        ],
+        [
+          "https://scholar.google.com/scholar?oi=bibs&hl=en&oe=ASCII&cites=2815146270502492897",
+          "2"
+        ]
+      ]
+    },
+    "year": "2008"
+  }
+];
